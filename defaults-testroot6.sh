39c7b64af77d816ea9a7fbffd743f9b9d3e2e73b
Dario Berzano <dario.berzano@cern.ch>
2019-04-01 11:53:10 +0200

    WIP ~ TESTROOT5/6

---
+++ defaults-testroot6.sh
@@ -0,0 +1,16 @@
+package: defaults-next-root6
+version: v1
+disable:
+  - arrow
+env:
+  CXXFLAGS: "-fPIC -g -O2 -std=c++11"
+  CFLAGS: "-fPIC -g -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+overrides:
+  AliRoot:
+    version: "%(tag_basename)s_ROOT6"
+    tag: v5-09-47a
+  AliPhysics:
+    version: "%(tag_basename)s_ROOT6"
+    tag: v5-09-47a-01
+---
